1dfe18edc68ce8dc4ba7265747c53fb8ec363c0b
yakravtsova <margaritaselez@yandex.ru>
2023-03-03 00:27:07 +0500

    some config changes

--- views/node/App.js
+++ views/node/App.js
@@ -359,14 +359,10 @@ var _reactRedux = require("react-redux");
 var _projectSlice = require("../src/features/projects/projectSlice");
 var _statusSlice = require("../src/features/statuses/statusSlice");
 var _issueSlice = require("../src/features/issues/issueSlice");
-const _excluded = ["id"],
-  _excluded2 = ["id"];
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 const SearchForm = ({
   setIsFirstSearch
 }) => {
@@ -426,37 +422,31 @@ const SearchForm = ({
     name: "project",
     label: "Select a project",
     defaultValue: []
-  }, _ref => {
-    let {
-        fieldProps: {
-          id
-        }
-      } = _ref,
-      rest = _objectWithoutProperties(_ref.fieldProps, _excluded);
-    return /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement(_select.default, _extends({
-      inputId: id
-    }, rest, {
-      options: project.projects,
-      isMulti: true
-    })));
-  }), /*#__PURE__*/_react.default.createElement(_form.Field, {
+  }, ({
+    fieldProps: {
+      id,
+      ...rest
+    }
+  }) => /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement(_select.default, _extends({
+    inputId: id
+  }, rest, {
+    options: project.projects,
+    isMulti: true
+  })))), /*#__PURE__*/_react.default.createElement(_form.Field, {
     name: "status",
     label: "Select an issue status",
     defaultValue: []
-  }, _ref2 => {
-    let {
-        fieldProps: {
-          id
-        }
-      } = _ref2,
-      rest = _objectWithoutProperties(_ref2.fieldProps, _excluded2);
-    return /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement(_select.default, _extends({
-      inputId: id
-    }, rest, {
-      options: status.statuses,
-      isMulti: true
-    })));
-  }), /*#__PURE__*/_react.default.createElement(_form.Field, {
+  }, ({
+    fieldProps: {
+      id,
+      ...rest
+    }
+  }) => /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement(_select.default, _extends({
+    inputId: id
+  }, rest, {
+    options: status.statuses,
+    isMulti: true
+  })))), /*#__PURE__*/_react.default.createElement(_form.Field, {
     name: "issuesPerPage",
     label: "Maximum number of tasks per page",
     defaultValue: "50",
